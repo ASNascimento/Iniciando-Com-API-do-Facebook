4a11b319f300d224e450447809b1bf179d1cf18e
Alex Nascimento <contato@ansolutions.com.br>
2016-10-26 15:06:53 -0200

    Update Config.php

--- aula05/_app/Config.php
+++ aula05/_app/Config.php
@@ -4,8 +4,8 @@ session_start();
 require_once 'Facebook/autoload.php';
 
 $fb = new Facebook\Facebook([
-    'app_id' => '1749977245264591',
-    'app_secret' => 'd2586ad2a80121c36f2e6dea340f80ff',
+    'app_id' => '{ID do Aplicativo}',
+    'app_secret' => '{Chave Secreta do Aplicativo}',
     'default_graph_version' => 'v2.7',
         ]);
 
